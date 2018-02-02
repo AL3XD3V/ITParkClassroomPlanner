38103f042c16d1a38c5e96a33760a66b57800bf7
AL3XD3V <real_slam@mail.ru>
2018-02-02 16:40:08 +0300

    first version of index.php + add css, font, logo

--- index.php
+++ index.php
@@ -3,8 +3,35 @@
   <head>
     <meta charset="utf-8">
     <title>Welcome!</title>
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
+    <div class="top-line">
+    </div>
+    <div class="nav-bar">
+      <div class="logo">
+        <img src="img/park-logo.png" alt="IT-Park">
+      </div>
+      <div class="menu">
+        <ul>
+          <li>
+            <a class="menu-link" href="classroom.php">Аудитории</a>
+          </li>
+          <li>
+            <a class="menu-link" href="request.php">Заявка</a>
+          </li>
+          <li>
+            <a class="menu-link" href="profile.php">Мой профиль</a>
+          </li>
+          <li>
+            <a class="menu-link" href="reg.php">Регистрация</a>
+          </li>
+          <li>
+            <a class="menu-link" href="login.php">Вход</a>
+          </li>
+        </ul>
+      </div>
+    </div>
     <p>Hello!</p>
   </body>
 </html>
