5b013b3dac9706c985842e9f0e0f43210e179255
AL3XD3V <real_slam@mail.ru>
2018-02-12 11:34:03 +0300

    add bootstrap 4

--- login.php
+++ login.php
@@ -2,23 +2,34 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Авторизация</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body>
-    <div class="top-line">
-    </div>
-    <div class="login_window">
-      <div class="logo">
-        <img src="img/park-logo.png" alt="IT-Park">
+    <div class="container">
+      <div class="row">
+        <div class="col" style="text-align: center; padding: 20px; padding-top: 150px;">
+          <img src="img/park-logo.png" style="border-radius: 5px;" alt="it-park">
+        </div>
       </div>
-      <form class="auth_form" action="auth.php" method="post">
-        <p><b>Логин</b></p>
-        <p><input type="text" name="login" value=""><Br>
-        <p><b>Пароль</b></p>
-        <p><input type="text" name="password" value=""><Br>
-        <p><input type="submit" value="Вход"></p>
-      </form>
+      <div class="row justify-content-center">
+        <div class="col-3">
+          <form>
+            <div class="form-group">
+              <input type="text" class="form-control" id="loginField" placeholder="Логин">
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" id="loginField" placeholder="Пароль">
+            </div>
+            <div class="form-group">
+              <input type="submit" class="btn btn-success btn-block" value="Войти">
+            </div>
+          </form>
+      </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
