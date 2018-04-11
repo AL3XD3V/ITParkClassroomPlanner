f927ca6d7dd8516b293139be4ea4f7f9409380ed
AL3XD3V <real_slam@mail.ru>
2018-04-11 16:53:16 +0300

    edit tables in profile view

--- src/views/view_profile.php
+++ src/views/view_profile.php
@@ -10,63 +10,53 @@
 		</div>
 	</div>
 </div>
-<div class="container">
-  <div class="row justify-content-center">
-    <b>Это вы:</b>
+<div class="container" style="margin-top: 50px;">
+  <div class="row justify-content-center" style="padding: 15px;">
+    <b>Мой профиль:</b>
   </div>
   <div class="row justify-content-center">
-      <div class="col-12">
-        <table class="table table-bordered">
-						<tr>
-      				<th>Фамилия</th>
-      				<th>Имя</th>
-      				<th>Отчество</th>
-      				<th>Отдел</th>
-							<th>Должность</th>
-							<th>Почта</th>
-							<th>Телефон</th>
-    				</tr>
+      <div class="col-6">
+        <table class="table borderless" >
 						<?php
 						foreach($data as $row)
 						{
-							echo '<tr><td>'.$row['surname'].'</td>'.
-									 	'<td>'.$row['name'].'</td>'.
-									 	'<td>'.$row['patron'].'</td>'.
-									 	'<td>'.$row['division'].'</td>'.
-									 	'<td>'.$row['position'].'</td>'.
-									 	'<td>'.$row['email'].'</td>'.
-									 	'<td>'.$row['phone'].'</td></tr>';
+							echo '<tr><td><p>'.$row['surname'].' '.$row['name'].' '.$row['patron'].
+									 	'<p>'.$row['division'].', '.$row['position'].'</p></td>'.
+									 	'<td><p>'.$row['email'].'</p>'.
+									 	'<p>'.$row['phone'].'</p></tr></td>';
 						}
 						?>
 					</table>
     		</div>
     </div>
 </div>
-<div class="container">
-		<div class="row justify-content-center">
-			<b>Это ваши заявки:</b>
+<div class="container" style="margin-top: 50px; margin-bottom: 100px;">
+		<div class="row justify-content-center" style="padding: 15px;">
+			<b>Мои заявки:</b>
 		</div>
 		<div class="row justify-content-center">
     		<div class="col-12">
-          <table class="table table-bordered">
-						<tr>
-      				<th>День</th>
-      				<th>Аудитория</th>
-      				<th>Начало</th>
-      				<th>Окончание</th>
-							<th>Мероприятие</th>
-              <th>Подтверждено</th>
+          <table class="table table-bordered" style="text-align: center;">
+						<tr style="background: #CCC;">
+      				<th style="width: 10%">День</th>
+      				<th style="width: 10%">Аудитория</th>
+      				<th style="width: 20%">Время проведения</th>
+							<th style="width: 50%">Название мероприятия</th>
+              <th style="width: 10%">Подтверждено</th>
     				</tr>
 						<?php
 						foreach($data2 as $row)
 						{
 
-								echo '<tr><td>'.$row['day'].'</td>'.
+								echo '<tr><td>'. date('d.m.Y', strtotime($row['day'])).'</td>'.
 									 	'<td>'.$row['class'].'</td>'.
-									 	'<td>'.substr($row['time_start'], 0, 5).'</td>'.
-									 	'<td>'.substr($row['time_stop'], 0, 5).'</td>'.
-                    '<td>'.$row['name'].'</td>'.
-                    '<td>'.$row['confirm'].'</td></tr>';
+									 	'<td>'.substr($row['time_start'], 0, 5).' - '.substr($row['time_stop'], 0, 5).'</td>'.
+                    '<td>'.$row['name'].'</td>';
+                  if ($row['confirm'] == 0) {
+										echo '<td> - </td></tr>';
+									}  else {
+										echo '<td><img src="img/ok.png" class="ok_picture" width="20" alt="Подтверждено"></td></tr>';
+									}
 						}
 						?>
 					</table>
